044189b4695069d79bdec3e0527baab519391f4e
Lihan Zhang <lihan.bibi.zhang@gmail.com>
2018-06-10 01:21:25 -0400

    Add new credentials file

--- SGBot.js
+++ SGBot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const config = require('./config.json');
+const config = require('../config');
+const credentials = require('../credentials')
 
 bot.login(config.token);
 
