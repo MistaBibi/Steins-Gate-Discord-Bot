e5d613dfccd1da04c997e628a5d9599f64a91093
Lihan Zhang <lihan.bibi.zhang@gmail.com>
2018-06-13 00:59:50 -0400

    Refacor code; Resolves #1
    - Refactor code in accordance with issues brought up in the issue
    - Add custom emoji pngs!

--- SGBot.js
+++ SGBot.js
@@ -1,27 +1,28 @@
+'use strict';
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require('./config.json');
-const credentials = require('./credentials.json')
+const credentials = require('./credentials.json');
 const utils = require('./utils.js');
 
 bot.login(credentials.token);
 
 bot.on('ready', () => {
     // bot.user.setUsername('Emoji Microwave (Temporary Name)')
-    console.log("Awaiting D-mails!");
-    bot.user.setPresence({ game: { name: 'with worldlines' }, status: 'online' })
+    console.log('Awaiting D-mails!');
+    bot.user.setPresence({ game: { name: 'with worldlines' }, status: 'online' });
 });
 
 bot.on('message', (message) => {
 
     // String literal matches
-    if(message.content.trim().toLowerCase() == 'nullpo') {
+    if(message.content.toLowerCase() === 'nullpo') {
         message.channel.send('Gah!');
     }
 
     // Commands
-    if(message.content.trim().startsWith(config.commandPrefix)) {
-        command = utils.parseCommand(message.content);
+    if(message.content.startsWith(config.commandPrefix)) {
+        const command = utils.parseCommand(message.content);
         switch (command.toLowerCase()) {
             case undefined:
             case 'help':
@@ -30,7 +31,7 @@ bot.on('message', (message) => {
     }
 
     // Emojis
-    if(message.content.trim().includes(":")) {
+    if(message.content.includes(':')) {
         emojiCommand(message);
     }
 });
@@ -38,23 +39,26 @@ bot.on('message', (message) => {
 function helpCommand (message) {
     const emojiEntries = Object.entries(config.emojis).map(([emoji, entry]) => `${config.emojiPrefix}${emoji}\n\t- ${entry.description}`);
     const commandEntries = Object.entries(config.commands).map(([command, entry]) => `${config.commandPrefix}${command} ${entry.usage}\n\t- ${entry.description}`);
-
     const helpText =
-    `I-It's not like I want you to know how I work or anything, **b-b-baka**!\n_Emojis:_\n${emojiEntries.join('\n')}\n_Commands:_\n${commandEntries.join('\n')}`
+`I-It's not like I want you to know how I work or anything, **b-b-baka**!
+_Emojis:_
+${emojiEntries.join('\n')}
+_Commands:_
+${commandEntries.join('\n')}`;
 
     message.channel.send(helpText);
 }
 
 function emojiCommand (message) {
-    let emojiPrefix = config.emojiPrefix;
-    let messageParts = message.content.split(" ");
-    emoji = utils.findEmojiLocation(`${emojiPrefix}([^\s]*)`, messageParts)
-
-    for(let emojiEntry of Object.entries(config.emojis)) {
-        if(emoji.match(`${emojiPrefix}${emojiEntry[0]}([^\s]*)`)) {
-            message.channel.send("", {
-                file: `${emojiEntry[1].filePath}`
-            })
+    const emojiPrefix = config.emojiPrefix;
+    
+    let match;
+    if(match = message.content.match(`${emojiPrefix}([^\\s]*)`)) {
+        const [, emoji] = match;
+        if(config.emojis[emoji]) {
+            message.channel.send('', {
+                file: `${config.emojis[emoji].filePath}`
+           });
         }
     }
 }
