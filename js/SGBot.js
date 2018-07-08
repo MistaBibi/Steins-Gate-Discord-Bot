55e9e95e79c6e5b05c35f8b1948968a9aa21c82a
Kyle Herock <kherock@gmail.com>
2018-07-08 10:38:33 -0400

    Avoid unhandled promise rejections for login and event handlers

--- js/SGBot.js
+++ js/SGBot.js
@@ -1,56 +1,65 @@
 'use strict';
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const utils = require('./utils.js');
 const config = require('../config.json');
 const credentials = require('../credentials.json');
-const utils = require('../js/utils.js');
 
-bot.login(credentials.token);
-
-bot.on('ready', () => {
-    // bot.user.setUsername('Emoji Microwave (Temporary Name)')
-    console.log('Awaiting D-mails!');
-    bot.user.setPresence({ game: { name: 'with worldlines' }, status: 'online' });
+const bot = new Discord.Client();
+bot.login(credentials.token).catch(err => {
+    console.error(err.message);
+    bot.destroy();
 });
 
-bot.on('message', (message) => {
+bot.on('ready', async () => {
+    try {
+        // await bot.user.setUsername('Emoji Microwave (Temporary Name)')
+        await bot.user.setPresence({ game: { name: 'with worldlines' }, status: 'online' });
+    } catch(err) {
+        console.error(err.stack);
+    }
+    console.log('Awaiting D-mails!');
+});
 
+bot.on('message', async (message) => {
     // No-op if message was from a bot
     if(message.author.bot) return;
 
-    // String literal matches
-    if(message.content.toLowerCase() === 'nullpo') {
-        message.channel.send('Gah!');
-    }
-
-    // Commands
-    if(message.content.startsWith(config.commandPrefix)) {
-        const command = utils.parseCommand(message.content);
-        switch(command.toLowerCase()) {
-            case 'help':
-                helpCommand(message);
-                break;
-            case 'tuturu':
-                playCommand(message, command);
-                break;
-            case 'beechga':
-                playCommand(message, command);
-                break;
-            case 'sonuvabetch':
-                playCommand(message, command);
-                break;
+    try {
+        // String literal matches
+        if(message.content.toLowerCase() === 'nullpo') {
+            await message.channel.send('Gah!');
         }
-    }
 
-    // Emojis
-    if(message.content.includes(':')) {
-        emojiCommand(message);
+        // Commands
+        if(message.content.startsWith(config.commandPrefix)) {
+            const command = utils.parseCommand(message.content);
+            switch(command.toLowerCase()) {
+                case 'help':
+                    await helpCommand(message);
+                    break;
+                case 'tuturu':
+                case 'beechga':
+                case 'sonuvabetch':
+                    await playCommand(message, command);
+                    break;
+            }
+        }
+
+        // Emojis
+        if(message.content.includes(':')) {
+            await emojiCommand(message);
+        }
+    } catch(err) {
+        console.error(err.stack);
     }
 });
 
 function helpCommand (message) {
-    const emojiEntries = Object.entries(config.emojis).map(([emoji, entry]) => `${config.emojiPrefix}${emoji}\n\t- ${entry.description}`);
-    const commandEntries = Object.entries(config.commands).map(([command, entry]) => `${config.commandPrefix}${command} ${entry.usage}\n\t- ${entry.description}`);
+    const emojiEntries = Object.entries(config.emojis)
+        .map(([emoji, entry]) => `${config.emojiPrefix}${emoji}\n\t- ${entry.description}`);
+    const commandEntries = Object.entries(config.commands)
+        .map(([command, entry]) => `${config.commandPrefix}${command} ${entry.usage}\n\t- ${entry.description}`);
+
     const helpText =
 `I-It's not like I want you to know how I work or anything, **b-b-baka**!
 _Emojis:_
@@ -58,15 +67,14 @@ ${emojiEntries.join('\n')}
 _Commands:_
 ${commandEntries.join('\n')}`;
 
-    message.channel.send(helpText);
+    return message.channel.send(helpText);
 }
 
 function playCommand (message, commandName) {
-    const targetChannel = message.member.voiceChannel;
-    if(!targetChannel) message.channel.send(config.commands[commandName].description);
-    else {
-        utils.playAudioFile(targetChannel, commandName)
-    }
+    const { voiceChannel } = message.member;
+    return voiceChannel
+        ? utils.playAudioFile(voiceChannel, commandName)
+        : message.channel.send(config.commands[commandName].description);
 }
 
 function emojiCommand (message) {
--- js/utils.js
+++ js/utils.js
@@ -4,11 +4,10 @@ exports.parseCommand = function (messageContent) {
     return messageContent.trim().slice(config.commandPrefix.length);
 }
 
-exports.playAudioFile = function (targetChannel, commandName) {
-    targetChannel.join().then(connection => {
-        const dispatcher = connection.playFile(config.commands[commandName].filePath);
-        dispatcher.on("end", end => {
-            targetChannel.leave();
-        });
-    }).catch(console.error);
+exports.playAudioFile = async function (targetChannel, commandName) {
+    const connection = await targetChannel.join();
+    const dispatcher = connection.playFile(config.commands[commandName].filePath);
+    dispatcher.on('end', end => {
+        targetChannel.leave();
+    });
 }
